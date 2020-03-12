77b0feaa2b3207a0e7df45fdcc7f8781e71824ec
Samuel <Samuel.Painchaud@usherbrooke.ca>
2020-03-11 22:38:52 -0400

    Add mainmenu and set login to default page

---
+++ epmusmobile/app/src/main/java/com/example/epmusmobile/MainMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.epmusmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
